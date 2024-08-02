1daf8b2a437b0c448878cc4fedcb1e60926654ef
saprabh20 <saprabhanand20@gmail.com>
2024-08-02 14:36:43 +0530

    3 slides hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,28 +44,6 @@ const Hero = () => {
                             alt="..."
                         />
                     </div>
-                    {/* Item 4 */}
-                    <div
-                        className="hidden duration-700 ease-in-out"
-                        data-carousel-item
-                    >
-                        <img
-                            src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/pball.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9wYmFsbC5qcGciLCJpYXQiOjE3MjI1ODg3NzMsImV4cCI6MTc1NDEyNDc3M30.yvvGwRuDBitQAoQqoQZhmkgUuIzfm63xRc_Zz1p3FEw&t=2024-08-02T08%3A52%3A50.099Z"
-                            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="..."
-                        />
-                    </div>
-                    {/* Item 5 */}
-                    <div
-                        className="hidden duration-700 ease-in-out"
-                        data-carousel-item
-                    >
-                        <img
-                            src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/pball.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9wYmFsbC5qcGciLCJpYXQiOjE3MjI1ODg3NzMsImV4cCI6MTc1NDEyNDc3M30.yvvGwRuDBitQAoQqoQZhmkgUuIzfm63xRc_Zz1p3FEw&t=2024-08-02T08%3A52%3A50.099Z"
-                            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="..."
-                        />
-                    </div>
                 </div>
                 {/* Slider indicators */}
                 <div className="absolute z-40 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
@@ -89,20 +67,6 @@ const Hero = () => {
                         aria-current="false"
                         aria-label="Slide 3"
                         data-carousel-slide-to={2}
-                    />
-                    <button
-                        type="button"
-                        className="w-3 h-3 rounded-full"
-                        aria-current="false"
-                        aria-label="Slide 4"
-                        data-carousel-slide-to={3}
-                    />
-                    <button
-                        type="button"
-                        className="w-3 h-3 rounded-full"
-                        aria-current="false"
-                        aria-label="Slide 5"
-                        data-carousel-slide-to={4}
                     />
                 </div>
                 {/* Slider controls */}
