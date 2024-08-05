3cc2f3531628465d2b8693f9bc76b2940f840b43
saprabh20 <saprabhanand20@gmail.com>
2024-08-06 03:01:21 +0530

    supabase implementation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,51 @@
 import { Carousel } from "flowbite-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import supabase from "../assets/supabseClient";
 
 const Hero = () => {
+    // const [carouselImages, setCarouselImages] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchProducts = async () => {
+    //         const { data, error } = await supabase.storage.from("images").list("hero");
+
+    //         if (error) {
+    //             console.error("Error fetching products:", error);
+    //         } else {
+    //             // setCarouselImages(...carouselImages, data);
+    //             console.log(data);
+    //         }
+    //     };
+
+    //     fetchProducts();
+    // }, []);
     return (
         <div className="h-96 md:h-[600px] flex items-center justify-center">
             <Carousel className="rounded-none" slideInterval={3000}>
                 <img
                     className="rounded-none"
-                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/Handicraft%20Janpath/cropped-image%20(1).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9IYW5kaWNyYWZ0IEphbnBhdGgvY3JvcHBlZC1pbWFnZSAoMSkucG5nIiwiaWF0IjoxNzIyNTg4NjQ4LCJleHAiOjE3NTQxMjQ2NDh9.wchcFSUoS_uEw74ysAHXbnruU_6KCNX9oyYj1kKix6c&t=2024-08-02T08%3A50%3A45.710Z"
+                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/public/images/hero/margarita1.png"
                     alt="..."
                 />
                 <img
-                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/Handicraft%20Janpath/cropped-image%20(14).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9IYW5kaWNyYWZ0IEphbnBhdGgvY3JvcHBlZC1pbWFnZSAoMTQpLnBuZyIsImlhdCI6MTcyMjU4ODcwNCwiZXhwIjoxNzU0MTI0NzA0fQ.mCbORhNznXiQXEoo2m_x3ld2O1tfVO5QJsv1C8D5MrA&t=2024-08-02T08%3A51%3A41.282Z"
+                    className="rounded-none"
+                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/public/images/hero/circleorange4.png"
                     alt="..."
                 />
                 <img
-                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/Handicraft%20Janpath/cropped-image%20(2).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9IYW5kaWNyYWZ0IEphbnBhdGgvY3JvcHBlZC1pbWFnZSAoMikucG5nIiwiaWF0IjoxNzIyNTg4NzM3LCJleHAiOjE3NTQxMjQ3Mzd9.CCJfL5D8sorJwi0LEpmsTvbFAZKHyIZ-al5sw-2-PVU&t=2024-08-02T08%3A52%3A14.663Z"
+                    className="rounded-none"
+                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/public/images/hero/bigmix1.png"
                     alt="..."
                 />
                 <img
-                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/pball.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9wYmFsbC5qcGciLCJpYXQiOjE3MjI3MTAyNDYsImV4cCI6MTc1NDI0NjI0Nn0.awsgS4_X-6asCDrlHL6hwnDbguoQ5VplmybJ7xwZb1E&t=2024-08-03T18%3A37%3A22.850Z"
+                    className="rounded-none"
+                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/public/images/hero/smiley1.png"
                     alt="..."
                 />
                 <img
-                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/sign/products/pball.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwcm9kdWN0cy9wYmFsbC5qcGciLCJpYXQiOjE3MjI3MTAyNDYsImV4cCI6MTc1NDI0NjI0Nn0.awsgS4_X-6asCDrlHL6hwnDbguoQ5VplmybJ7xwZb1E&t=2024-08-03T18%3A37%3A22.850Z"
+                    className="rounded-none"
+                    src="https://stjrdjxugpuvmvhqobik.supabase.co/storage/v1/object/public/images/hero/circleblue3.png"
                     alt="..."
                 />
             </Carousel>
